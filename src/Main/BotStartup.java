6b288b7e1d720068ab66d9e8f9c5d33bc96f79af
Aylesworth <nguyenducanh2105@gmail.com>
2022-01-15 02:26:54 +0700

    New Update

--- src/Main/BotStartup.java
+++ src/Main/BotStartup.java
@@ -5,9 +5,6 @@ import javax.security.auth.login.LoginException;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.utils.ChunkingFilter;
-import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 public class BotStartup {
 	public static void main(String[] args) throws LoginException {
