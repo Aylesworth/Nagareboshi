5df96bf0592f8a11e77dd8e55c13b6667ded6fe0
Aylesworth <nguyenducanh2105@gmail.com>
2022-01-25 14:34:44 +0700

    Update ChangePrefixCommand.java

--- src/main/java/aylesw/meteor/command/commands/ChangePrefixCommand.java
+++ src/main/java/aylesw/meteor/command/commands/ChangePrefixCommand.java
@@ -3,6 +3,7 @@ package aylesw.meteor.command.commands;
 import aylesw.meteor.Config;
 import aylesw.meteor.command.CommandContext;
 import aylesw.meteor.command.ICommand;
+import net.dv8tion.jda.api.Permission;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,6 +11,11 @@ import java.util.List;
 public class ChangePrefixCommand implements ICommand {
     @Override
     public void handle(CommandContext ctx) {
+        if (!ctx.getMember().hasPermission(Permission.ADMINISTRATOR)) {
+            ctx.getChannel().sendMessage("You don't have enough permissions to use this command.").queue();
+            return;
+        }
+
         String newPrefix = ctx.getArgs().get(0);
         Config.setPrefix(newPrefix);
         ctx.getChannel().sendMessageFormat("Prefix changed to `%s`.", newPrefix).queue();
